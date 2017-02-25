a08dea0f2eb2502e5b035e2a1e43c779fad1144d
Jake Vandermeer <jvmeer@aprille.local>
2017-02-24 20:01:08 -0500

    Initial Commit

---
+++ Crossworder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Crossworder
+//
+//  Created by Jacob Vandermeer on 2/24/17.
+//  Copyright © 2017 Jacob Vandermeer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
